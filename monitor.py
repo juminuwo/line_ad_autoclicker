203d0bf5ce0abe74b36f5757c09b887909756c22
Adrian Liu <howismypielola@gmail.com>
2020-01-27 14:29:15 +0000

    quest button press because ads fuck it up

--- monitor.py
+++ monitor.py
@@ -1,14 +1,13 @@
 if __name__ == '__main__':
     import time
 
-
     import pyscreenshot
     from pynput import keyboard, mouse
 
-    from utils import check_ad_ready, check_ad_screen, image_loader, mse
+    from utils import check_ad_ready, check_ad_screen, image_loader, mse, check_main_page
 
     res = pyscreenshot.grab().size
-    _, ad_not_ready_1, ad_ready_1, _ = image_loader()
+    _, ad_not_ready_1, ad_ready_1, _, main = image_loader()
 
     Mouse = mouse.Controller()
     Keyboard = keyboard.Controller()
@@ -22,19 +21,27 @@ if __name__ == '__main__':
 
     while True:
         time.sleep(5)
-        if check_ad_screen(ad_ready_1, ad_not_ready_1):
-            if check_ad_ready(ad_ready_1):
-                # click on ad
-                Mouse.position = (res[0] / 2, res[1] / 2)
-                Mouse.click(mouse.Button.left)
+        if check_main_page(main, res):
+            # click main page then click ad page
+            Mouse.position = (res[0] * 0.5286, res[1] * 0.0388)
+            Mouse.click(mouse.Button.left)
+            time.sleep(5)
+            Mouse.position = (res[0] * 0.6217, res[1] * 0.4592)
+            Mouse.click(mouse.Button.left)
+        else:
+            if check_ad_screen(ad_ready_1, ad_not_ready_1):
+                if check_ad_ready(ad_ready_1):
+                    # click on ad
+                    Mouse.position = (res[0] / 2, res[1] / 2)
+                    Mouse.click(mouse.Button.left)
+                else:
+                    # refresh page
+                    Keyboard.press(keyboard.Key.esc)
+                    Keyboard.release(keyboard.Key.esc)
+                    time.sleep(60)
+                    Mouse.position = (res[0] * 0.6217, res[1] * 0.4592)
+                    Mouse.click(mouse.Button.left)
             else:
-                # refresh page
+                # spam back to get out of ad page
                 Keyboard.press(keyboard.Key.esc)
                 Keyboard.release(keyboard.Key.esc)
-                time.sleep(5)
-                Mouse.position = (res[0] * 0.6217, res[1] * 0.4592)
-                Mouse.click(mouse.Button.left)
-        else:
-            # spam back to get out of ad page
-            Keyboard.press(keyboard.Key.esc)
-            Keyboard.release(keyboard.Key.esc)
--- utils.py
+++ utils.py
@@ -23,7 +23,8 @@ def image_loader():
     ad_not_ready_1 = Image.open('img/ad_not_ready_1.png')
     ad_ready_1 = Image.open('img/ad_ready_1.png')
     ad_ready_2 = Image.open('img/ad_ready_2.png')
-    imgs = (ads_complete, ad_not_ready_1, ad_ready_1, ad_ready_2)
+    main = Image.open('img/main.png')
+    imgs = (ads_complete, ad_not_ready_1, ad_ready_1, ad_ready_2, main)
     return imgs
 
 
@@ -46,12 +47,24 @@ def check_ad_screen(ad_ready_1, ad_not_ready_1):
         return False
 
 
+def check_main_page(main, res):
+    im = pyscreenshot.grab()
+    im = im.crop(
+        (res[0] * 0.5225, res[1] * 0.03, res[0] * 0.5325, res[1] * 0.04))
+    m = main.crop(
+        (res[0] * 0.5225, res[1] * 0.03, res[0] * 0.5325, res[1] * 0.04))
+    d = mse(np.array(im), np.array(m))
+    if d < 500:
+        return True
+    else:
+        return False
+
 if __name__ == '__main__':
     import time
-    ad_finished, ad_not_ready_1, ad_ready_1, ad_ready_2 = image_loader()
+    ad_finished, ad_not_ready_1, ad_ready_1, ad_ready_2, main = image_loader()
+    res = (1280, 800)
 
     while True:
         im = pyscreenshot.grab()
-        d = mse(np.array(im), np.array(ad_ready_1))
-        print(d)
-        time.sleep(5)
+        check_main_page(main, (1280, 800))
+        time.sleep(1)
